c43ea82177b70c4f2c82075ab1ba9d2adc0d55a6
Lili Baker <lilijbaker@gmail.com>
2022-04-28 14:26:26 -0400

    Update names of obstacles to reflect sprites

--- main.py
+++ main.py
@@ -12,19 +12,21 @@ if __name__ == '__main__':
     # Initialize world
     CUR_WORLD = MaoRun()
     game = Game()
-    control = ObstacleController()
     # Initialize player
 
     # Initialize obstacles
+    obstacles = [Obstacle("spades", game.obstacle_actions), Obstacle("diamonds", game.obstacle_actions), Obstacle("hearts", game.obstacle_actions), Obstacle("clubs", game.obstacle_actions)]
 
     # Initialize controller
+    control = ObstacleController()
 
     while True:
         # for event in pygame.event.get():
         #     if event.type == pygame.QUIT:
         #         pygame.quit()
         #         sys.exit()
-        obstacle_test = Obstacle("square", game.obstacle_actions)
-        control.interpret_input(obstacle_test.action)
+        control.interpret_input(obstacles[0].action, obstacles)
+        control.check_collision(obstacles[0])
+        obstacles[0].update_position()
 
         CUR_WORLD.display()
