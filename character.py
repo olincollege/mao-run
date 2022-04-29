7c4c0800261a54aa06696d71429ad879c88737a4
Cherry Pham <meokkmun@gmail.com>
2022-04-28 22:59:45 -0400

    Refactor character into GroupSingle

--- character.py
+++ character.py
@@ -7,6 +7,5 @@ class Character(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((800,300))
-        self.image.fill('Black')
-        self.rect = self.image.get_rect(midbottom = (200,300))
+        self.image = pygame.image.load("Sprites/joker2.png")
+        self.rect = self.image.get_rect(midbottom = (80,300))
--- main.py
+++ main.py
@@ -3,18 +3,22 @@ import pygame
 from random import choice
 from world import MaoRun
 from game import Game
+from character import Character
 from obstacle import Obstacle
 from controller import ObstacleController
 
 if __name__ == '__main__':
     pygame.init()
 
+    player = pygame.sprite.GroupSingle()
+    player.add(Character())
+
     # Initialize world
-    CUR_WORLD = MaoRun()
+    CUR_WORLD = MaoRun(player)
     game = Game()
 
     # Initialize player
-
+    
     # Initialize current obstacle
     current_obstacle = Obstacle(choice(game.possible_obstacles), game.obstacle_actions)
 
