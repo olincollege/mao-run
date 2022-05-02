e62763f9c57dac47c9446ee40fde58d2b06c28c4
Lili Baker <lilijbaker@gmail.com>
2022-05-02 13:26:28 -0400

    Add tests for analyzing input from keyboard

--- test_controller.py
+++ test_controller.py
@@ -2,6 +2,8 @@
 Unit tests for controller
 """
 
+from cgi import test
+from turtle import down, up
 import pytest
 import pygame
 from random import choice
@@ -9,35 +11,61 @@ from controller import ObstacleController
 from obstacle import Obstacle
 from game import Game
 
+pygame.init()
+
 # define variables needed to create test instances
-actions = [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_DOWN]
+actions = [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]
 possible_obstacles = ["spades", "diamonds", "hearts", "clubs"]
 obstacle_actions = {
-    "spades": choice(actions),
-    "diamonds": choice(actions),
-    "hearts": choice(actions),
-    "clubs": choice(actions),
+    "spades": pygame.K_UP,
+    "diamonds": pygame.K_DOWN,
+    "hearts": pygame.K_LEFT,
+    "clubs": pygame.K_RIGHT,
 }
 
 # define controller and obstacle for testing
 test_controller = ObstacleController()
-test_obstacle = Obstacle(choice(possible_obstacles), obstacle_actions)
+test_obstacles = [Obstacle(obstacle, obstacle_actions) for obstacle in possible_obstacles]
+test_spades = test_obstacles[0]
 
-# how do you test when you need to have the key go down??
-# def test_interpret_input():
-#     assert test_controller.interpret_input(test_obstacle.action, test_obstacle.action) == True
+# set up for testing with user input
+# up_arrow = pygame.event.Event(pygame.KEYDOWN, {'mod': 0, 'scancode': 30, 'key': pygame.K_UP, 'unicode': 'a'})
+up_arrow = pygame.event.Event(pygame.KEYDOWN, {'unicode': '', 'key': 1073741906, 'mod': 4096, 'scancode': 82, 'window': None})
+down_arrow = pygame.event.Event(pygame.KEYDOWN, {'unicode': '', 'key': 1073741905, 'mod': 4096, 'scancode': 81, 'window': None})
+left_arrow = pygame.event.Event(pygame.KEYDOWN, {'unicode': '', 'key': 1073741904, 'mod': 4096, 'scancode': 80, 'window': None})
+right_arrow = pygame.event.Event(pygame.KEYDOWN, {'unicode': '', 'key': 1073741903, 'mod': 4096, 'scancode': 79, 'window': None})
 
+key_input = [up_arrow, down_arrow, left_arrow, right_arrow]
+
+# define test cases
+correct_input_test_cases = [(key_input[i], test_obstacles[i]) for i in range(len(key_input))]
+incorrect_input_test_cases = [(key_input[::-1][i], test_obstacles[i]) for i in range(len(key_input))]
+
+
+# input testing
+# test that the correct input is detected
+@pytest.mark.parametrize("key,obstacle", correct_input_test_cases)
+def test_interpret_input_correct(key, obstacle):
+    assert test_controller.interpret_input(key, obstacle.action) == True
+
+# test that the incorrect input is detected as incorrect
+@pytest.mark.parametrize("key,obstacle", incorrect_input_test_cases)
+def test_interpret_input_incorrect(key, obstacle):
+    assert test_controller.interpret_input(key, obstacle.action) == False
+
+
+# collision testing
 # test that the collision is detected when it should be
 def test_check_collision_happens():
-    test_obstacle._x_position = 200
-    assert test_controller.check_collision(test_obstacle) == True
+    test_spades._x_position = 200
+    assert test_controller.check_collision(test_spades) == True
 
 # test that the collision is not detected when it should not be
 def test_check_collision_does_not_happen():
-    test_obstacle._x_position = 500
-    assert test_controller.check_collision(test_obstacle) == False
+    test_spades._x_position = 500
+    assert test_controller.check_collision(test_spades) == False
 
 # test that the collision is detected when obstacle is past the player
 def test_check_collision_happens_at_zero():
-    test_obstacle._x_position = 0
-    assert test_controller.check_collision(test_obstacle) == True
+    test_spades._x_position = 0
+    assert test_controller.check_collision(test_spades) == True
