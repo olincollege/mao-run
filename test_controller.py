60fe4036d95a31e741787f5f5fc29fde5c57f18e
Lili Baker <lilijbaker@gmail.com>
2022-05-03 13:20:36 -0400

    Update controller testing to test for collisions on both sides

--- test_controller.py
+++ test_controller.py
@@ -22,9 +22,12 @@ obstacle_actions = {
 }
 
 # define controller and obstacle for testing
-test_controller = ObstacleController()
+test_controller = ObstacleController(obstacle_actions)
 test_obstacles = [Obstacle(obstacle, obstacle_actions) for obstacle in possible_obstacles]
-test_spades = test_obstacles[0]
+test_spades_right = Obstacle("spades", obstacle_actions)
+test_spades_right._start_position = "right"
+test_spades_left = Obstacle("spades", obstacle_actions)
+test_spades_left._start_position = "left"
 
 # set up for testing with user input
 up_arrow = pygame.event.Event(pygame.KEYDOWN, {'unicode': '', 'key': 1073741906, 'mod': 4096, 'scancode': 82, 'window': None})
@@ -52,17 +55,24 @@ def test_interpret_input_incorrect(key, obstacle):
 
 
 # collision testing
-# test that the collision is detected when it should be
-def test_check_collision_happens():
-    test_spades._x_position = 200
-    assert test_controller.check_collision(test_spades) == True
+# test that the collision is detected when it should be from right
+def test_check_collision_happens_right():
+    test_spades_right._x_position = 400
+    assert test_controller.check_collision(test_spades_right) == True
 
 # test that the collision is not detected when it should not be
-def test_check_collision_does_not_happen():
-    test_spades._x_position = 500
-    assert test_controller.check_collision(test_spades) == False
+# from right hand side
+def test_check_collision_does_not_happen_right():
+    test_spades_right._x_position = 500
+    assert test_controller.check_collision(test_spades_right) == False
 
-# test that the collision is detected when obstacle is past the player
-def test_check_collision_happens_at_zero():
-    test_spades._x_position = 0
-    assert test_controller.check_collision(test_spades) == True
+# test that the collision is detected when it should be from left
+def test_check_collision_happens_left():
+    test_spades_left._x_position = 350
+    assert test_controller.check_collision(test_spades_left) == True
+
+# test that the collision is not detected when it should not be
+# from right hand side
+def test_check_collision_does_not_happen_left():
+    test_spades_left._x_position = 200
+    assert test_controller.check_collision(test_spades_left) == False
