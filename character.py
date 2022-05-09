4d25fc0f6299193ddb9abee4ceb400b76cea1abb
Lili Baker <lilijbaker@gmail.com>
2022-05-08 20:01:08 -0400

    Edit to conform to pylint

--- character.py
+++ character.py
@@ -29,19 +29,22 @@ class Character(pygame.sprite.Sprite):
         self.infoObject = pygame.display.Info()
         self.screen_width = self.infoObject.current_w/2
         self.screen_height = self.infoObject.current_h/2
-        
+
         # Load player images
         self.joker_1 = pygame.image.load("Sprites/joker2.png")
-        self.joker_1 = pygame.transform.scale(self.joker_1, (self.screen_width/5, self.screen_height/2.5))
+        self.joker_1 = pygame.transform.scale(self.joker_1, \
+            (self.screen_width/5, self.screen_height/2.5))
         self.joker_2 = pygame.image.load("Sprites/joker1.png")
-        self.joker_2 = pygame.transform.scale(self.joker_2, (self.screen_width/5, self.screen_height/2.5))
+        self.joker_2 = pygame.transform.scale(self.joker_2, \
+            (self.screen_width/5, self.screen_height/2.5))
 
         # Set up animation for player
         self.joker_moves = [self.joker_1, self.joker_2]
         self.joker_index = 0
 
         self.image = self.joker_moves[int(self.joker_index)]
-        self.rect = self.image.get_rect(midbottom = (self.screen_width/2, self.screen_height*.73))
+        self.rect = self.image.get_rect(midbottom = \
+            (self.screen_width/2, self.screen_height*.73))
 
     def joker_animation(self):
         """
