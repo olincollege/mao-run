c3f8ed0120dcc0f63f7464143f5a70f682d790b1
Lili Baker <lilijbaker@gmail.com>
2022-05-08 21:48:02 -0400

    Add attribute info to docstring

--- character.py
+++ character.py
@@ -9,15 +9,16 @@ class Character(pygame.sprite.Sprite):
     Create the character sprite for the game.
 
     Attributes:
-        infoObject:
-        screen_width:
-        screen_height:
-        joker_1:
-        joker_2:
-        joker_moves:
-        joker_index:
-        image:
-        rect:
+        info_object: Pygame VideoInfo instance representing current
+            graphics environment.
+        screen_width: An integer representing the screen width.
+        screen_height: An integer representing the screen height.
+        joker_1: A pygame surface representing the joker.
+        joker_2: A pygame surface representing the joker.
+        joker_moves: A list containing joker_1 and joker_2.
+        joker_index: An integer used to animate the joker.
+        image: A pygame surface representing the current joker.
+        rect: A pygame rectangle for defining position of joker.
     """
 
     def __init__(self):
@@ -26,25 +27,27 @@ class Character(pygame.sprite.Sprite):
         """
         super().__init__()
         # Fetch screen info
-        self.infoObject = pygame.display.Info()
-        self.screen_width = self.infoObject.current_w/2
-        self.screen_height = self.infoObject.current_h/2
+        self.info_object = pygame.display.Info()
+        self.screen_width = self.info_object.current_w/2
+        self.screen_height = self.info_object.current_h/2
 
         # Load player images
         self.joker_1 = pygame.image.load("Sprites/joker2.png")
-        self.joker_1 = pygame.transform.scale(self.joker_1, \
-            (self.screen_width/5, self.screen_height/2.5))
+        self.joker_1 = pygame.transform.scale(self.joker_1,
+                                              (self.screen_width/5,
+                                              self.screen_height/2.5))
         self.joker_2 = pygame.image.load("Sprites/joker1.png")
-        self.joker_2 = pygame.transform.scale(self.joker_2, \
-            (self.screen_width/5, self.screen_height/2.5))
+        self.joker_2 = pygame.transform.scale(self.joker_2,
+                                              (self.screen_width/5,
+                                              self.screen_height/2.5))
 
         # Set up animation for player
         self.joker_moves = [self.joker_1, self.joker_2]
         self.joker_index = 0
 
         self.image = self.joker_moves[int(self.joker_index)]
-        self.rect = self.image.get_rect(midbottom = \
-            (self.screen_width/2, self.screen_height*.73))
+        self.rect = self.image.get_rect(midbottom=(
+            self.screen_width/2, self.screen_height*.73))
 
     def joker_animation(self):
         """
